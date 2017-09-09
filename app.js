f3a903d70f997ed482df415c05334337fe41c1b4
Robert Nguyen <ng.hung83@gmail.com>
2017-09-09 12:55:26 +0700

    Update 'help' command

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var builder = require('botbuilder');
 //=========================================================
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 8080, function () {
-   console.log('%s listening to %s', server.name, server.url);
+server.listen(process.env.port || process.env.PORT || 8080, function() {
+    console.log('%s listening to %s', server.name, server.url);
 });
 // Create chat bot
 var connector = new builder.ChatConnector({
@@ -16,36 +16,41 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 //Bot on
-bot.on('contactRelationUpdate', function (message) {
+bot.on('contactRelationUpdate', function(message) {
     if (message.action === 'add') {
         var name = message.user ? message.user.name : null;
         var reply = new builder.Message()
-                .address(message.address)
-                .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'there');
+            .address(message.address)
+            .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'there');
         bot.send(reply);
     } else {
         // delete their data
     }
 });
-bot.on('typing', function (message) {
-  // User is typing
+bot.on('typing', function(message) {
+    // User is typing
 });
-bot.on('deleteUserData', function (message) {
+bot.on('deleteUserData', function(message) {
     // User asked to delete their data
 });
 //=========================================================
 // Bots Dialogs
 //=========================================================
-String.prototype.contains = function(content){
-  return this.indexOf(content) !== -1;
+String.prototype.contains = function(content) {
+    return this.indexOf(content) !== -1;
 };
 
-bot.dialog('/', function (session) {
-    if(session.message.text.toLowerCase().contains('hello')){
-      session.send(`Hey, How are you?`);
-      }else if(session.message.text.toLowerCase().contains('help')){
-        session.send(`How can I help you?`);
-      }else{
+bot.dialog('/', function(session) {
+    if (session.message.text.toLowerCase().contains('hello')) {
+        session.send(`Hey, How are you?`);
+    } else if (session.message.text.toLowerCase().contains('help')) {
+        session.send(
+            `Usage:
+1. [FShare link]. Example: @iBot https://www.fshare.vn/file/2GWXN9YU2ENQ
+2. [Chiasenhac playlist link]. Example: @iBot http://chiasenhac.vn/nhac-hot/dusk-till-dawn~zayn-sia~tsvd53t0qmhwfn.html
+3. New stuff coming in pretty soon
+            `);
+    } else {
         session.send(`Sorry I don't understand you...`);
-      }
+    }
 });
